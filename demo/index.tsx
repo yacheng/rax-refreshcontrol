22e27aa056346195a5c889f169684d3f102eb7fb
浴风 <liangzhi.llz@alibaba-inc.com>
2019-07-26 16:28:39 +0800

    更新readme

--- demo/index.tsx
+++
@@ -1,7 +0,0 @@
-import {createElement, render} from 'rax';
-import * as DriverDOM from 'driver-dom';
-import * as DriverWeex from 'driver-weex';
-import { isWeex } from 'universal-env';
-import RefreshControl from '../src/index';
-
-render(<RefreshControl refreshing={false} />, document.body, { driver: isWeex ? DriverWeex : DriverDOM });
